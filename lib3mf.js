2d38f3e39cec2e3bfa4e01ccd6f9cd7280341fa3
Vijai Kumar S <2363451+vijaiaeroastro@users.noreply.github.com>
2024-10-19 11:32:58 +0200

    Reorganize libraries

--- lib3mf.js
+++ lib3mf.js
@@ -1,23 +1,21 @@
-// lib3mf.js
 const path = require('path');
+const os = require('os');
 
+// Determine the platform and library extension
+const platform = os.platform();
+let lib3mfPath;
 
-function getAllMethods(obj) {
-    let props = [];
-    let currentObj = obj;
-
-    do {
-        // Get all properties (including non-enumerable ones) from the current object
-        props = props.concat(Object.getOwnPropertyNames(currentObj));
-    } while (currentObj = Object.getPrototypeOf(currentObj));  // Go up the prototype chain
-
-    // Filter out non-function properties and remove duplicates
-    return props.sort().filter((e, i, arr) => typeof obj[e] === 'function' && e !== arr[i + 1]);
+if (platform === 'win32') {
+    // Windows uses .dll files
+    lib3mfPath = path.join(__dirname, 'libraries', 'lib3mf.dll');
+} else if (platform === 'darwin') {
+    // macOS uses .dylib files
+    lib3mfPath = path.join(__dirname, 'libraries', 'lib3mf.dylib');
+} else {
+    // Linux uses .so files
+    lib3mfPath = path.join(__dirname, 'libraries', 'lib3mf.so');
 }
 
-// Load the lib3mf Node.js addon
-const wrapper = require(path.join(__dirname, 'build/Release/3mf_nodeaddon.node'))(path.join(__dirname, 'lib3mf.so'));
-getAllMethods(wrapper)
-
-// Export the wrapper so it can be imported
-module.exports = wrapper;
+// Load the Node.js addon and the library path
+const wrapper = require(path.join(__dirname, 'build/Release/3mf_nodeaddon.node'))(lib3mfPath);
+module.exports = wrapper
